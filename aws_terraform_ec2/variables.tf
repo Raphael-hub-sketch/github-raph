754e5213c3ca10656af949a81aef4884ccbfe807
Raphael-hub-sketch <raphael+smitt@binadox.com>
2024-08-21 14:38:37 +0300

    Update variables.tf

--- aws_terraform_ec2/variables.tf
+++ aws_terraform_ec2/variables.tf
@@ -108,7 +108,7 @@ variable "multi_az_dictionary" {
 
 variable "dictionary_db_storage_size" {
   description = "The size of the database (Gb), minimum restriction by AWS is 20GB"
-  default     = 20
+  default     = 20000
 }
 
 variable "audit_db_class" {
