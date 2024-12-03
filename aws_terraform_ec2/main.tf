ce1597f5c0e5105ada82d366c261ac3059ecc9f3
Raphael-hub <raphael+smitt@binadox.com>
2024-12-03 13:45:40 +0300

    Update main.tf
    data "cloudinit_config" "example" > false false

--- aws_terraform_ec2/main.tf
+++ aws_terraform_ec2/main.tf
@@ -165,8 +165,8 @@ resource "aws_db_instance" "audit_db" {
 }
 
 data "cloudinit_config" "example" {
-  gzip          = true
-  base64_encode = true
+  gzip          = false
+  base64_encode = false
 
   part {
     content_type = "text/cloud-config"
