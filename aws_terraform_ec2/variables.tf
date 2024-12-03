bfe06b17940798f637593a12aa7b9f8c111b570a
Raphael-hub <raphael+smitt@binadox.com>
2024-12-03 13:44:20 +0300

    Update variables.tf
    CoEd 03+12+24 / 13+44

--- aws_terraform_ec2/variables.tf
+++ aws_terraform_ec2/variables.tf
@@ -129,7 +129,7 @@ variable "multi_az_audit" {
 
 variable "audit_db_storage_size" {
   description = "The size of the database (Gb), minimum restriction by AWS is 20GB"
-  default     = 555
+  default     = 8888
 }
 
 variable "db_username" {
