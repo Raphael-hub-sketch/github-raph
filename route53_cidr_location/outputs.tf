4faad72f611450172a7c9e72aadd524b6a159e7c
Raphael-hub <raphael+smitt@binadox.com>
2026-08-17 13:50:40 +0300

    Delete route53_cidr_location directory

--- route53_cidr_location/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "collection_id" {
-  description = "ID созданной коллекции CIDR"
-  value       = aws_route53_cidr_collection.main.id
-}
-
-output "collection_arn" {
-  description = "ARN созданной коллекции CIDR"
-  value       = aws_route53_cidr_collection.main.arn
-}
